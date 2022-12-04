fbcc2b348e992129609a5ac8f659064915ba4242
Fabio <fabio.karlino@gmail.com>
2022-12-04 20:45:53 +0100

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Alborgo Landing Page v1.0.0 (undefined)
+* Copyright 2013-2022 magusfabius
+* Licensed under MIT (https://github.com/StartBootstrap/alborgo/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
